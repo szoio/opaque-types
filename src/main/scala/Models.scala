84a06fd9768cb96c3052660597397b85ab590046
Stephen <stephenzoio@yahoo.com>
2022-10-21 10:59:22 +1100

    added custom codecs

--- src/main/scala/Models.scala
+++ src/main/scala/Models.scala
@@ -1,8 +1,21 @@
-import io.circe.Codec
+import io.circe.*
 import io.circe.generic.semiauto.*
 
-case class ReplicationFactor(value: Int)
-case class PartitionCount(value: Int)
+case class ReplicationFactor(value: Int) extends AnyVal
+
+object ReplicationFactor {
+  given Encoder[ReplicationFactor] = Encoder.encodeInt.contramap(_.value)
+  given Decoder[ReplicationFactor] = Decoder.decodeInt.map(apply)
+}
+
+case class PartitionCount(value: Int) extends AnyVal
+
+object PartitionCount {
+  given Encoder[PartitionCount] = Encoder.encodeInt.contramap(_.value)
+  given Decoder[PartitionCount] = Decoder.decodeInt.map(apply)
+}
+
+// https://docs.scala-lang.org/overviews/core/value-classes.html#limitations
 
 case class Topic(topicName: String, replicationFactor: ReplicationFactor, partitionCount: PartitionCount)
 
