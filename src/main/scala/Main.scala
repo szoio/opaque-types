104f93e289b2eaefc42028ddb79c1d2b556ec421
Stephen <stephenzoio@yahoo.com>
2022-10-21 10:41:59 +1100

    added type aliases

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,10 @@
 import io.circe.syntax._
 
 @main def main: Unit = {
-  val topic = Topic("my-topic", 3, 6)
+  val replicationFactor: ReplicationFactor = 3
+  val partitionCount: PartitionCount = 6
+
+  val topic = Topic("my-topic", replicationFactor, partitionCount)
 
   println(topic.asJson.spaces2)
 }
