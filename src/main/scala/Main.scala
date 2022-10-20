d89e30c7f54a24ea69036918e755d784e2f10fae
Stephen <stephenzoio@yahoo.com>
2022-10-21 10:59:22 +1100

    added custom codecs

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,4 +7,8 @@ import io.circe.syntax._
   val topic = Topic("my-topic", replicationFactor, partitionCount)
 
   println(topic.asJson.spaces2)
+
+  topic match {
+    case Topic(name, ReplicationFactor(r), PartitionCount(p)) => println(s"$name: $r, $p")
+  }
 }
