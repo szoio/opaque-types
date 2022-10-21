301485d5b9724bcb953269a6186ab9a7dcfa98d7
Stephen <stephenzoio@yahoo.com>
2022-10-21 15:43:46 +1100

    README and removed tests

--- src/main/scala/Models.scala
+++ src/main/scala/Models.scala
@@ -4,11 +4,7 @@ import io.circe.generic.semiauto.*
 type ReplicationFactor = Int
 type PartitionCount = Int
 
-<<<<<<< HEAD
-case class Topic(topicName: String, replicationFactor: Int, partitionCount: Int)
-=======
 case class Topic(topicName: String, replicationFactor: ReplicationFactor, partitionCount: PartitionCount)
->>>>>>> 658ecd7 (added type aliases)
 
 object Topic {
   given Codec[Topic] = deriveCodec
